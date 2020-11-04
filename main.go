ee059dfb89bf5d8ccd2ce2dbf0759d286a40d54c
Antoine Meillet <antoine.meillet@gmail.com>
2020-11-04 10:51:08 +0100

    support x-forwarded-for header
    Server will only display the content of the `x-forwarded-for`
    (case insensitive) header when it is set.

--- main.go
+++ main.go
@@ -9,15 +9,23 @@ import (
 )
 
 func getIP(w http.ResponseWriter, r *http.Request) {
-	ipr := regexp.MustCompile(`^(?:(?P<ipv4>[^:]+)|\[(?P<ipv6>.+)\]):(?P<port>\d+)$`)
-	ip := ipr.FindStringSubmatch(r.RemoteAddr)
-	if ip[1] == "" {
-		// ip[1] is empty when client uses IPv6
-		fmt.Fprintf(w, "%s\n", ip[2])
+	var client_ip string
+
+	forwarded := r.Header.Get("X-Forwarded-For")
+	if forwarded != "" {
+		client_ip = forwarded
 	} else {
-		fmt.Fprintf(w, "%s\n", ip[1])
-	}
+		ipr := regexp.MustCompile(`^(?:(?P<ipv4>[^:]+)|\[(?P<ipv6>.+)\]):(?P<port>\d+)$`)
+		ip := ipr.FindStringSubmatch(r.RemoteAddr)
 	
+		if ip[1] == "" {
+			// ip[1] is empty when client uses IPv6
+			client_ip = ip[2]
+		} else {
+			client_ip = ip[1]
+		}
+	}
+	fmt.Fprintf(w, "%s\n", client_ip)
 }
 
 func main() {
